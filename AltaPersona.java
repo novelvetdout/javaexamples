3590383f90d45d72f14dbbcfcc55aa2f0d64aadf
novelvetdout <loreyyy@gmail.com>
2015-05-09 09:39:14 -0400

    Create AltaPersona.java
    Créditos: Gabriela Soria

---
+++ AltaPersona.java
@@ -0,0 +1,21 @@
+package py.com.uaa.edu.pooj.practicaJ;
+//Ésta LDC importa parte de la libería .util: Scanner
+import java.util.Scanner;
+
+public class altaPersona {
+
+	public void ingresarDatosPersonales() {
+		Scanner userInput = new Scanner (System.in);
+		String[] listarDatos = new String[20];
+		for (int i = 1; i <21; i++ ){
+			System.out.println("Ingrese Nombre y Apellido:");
+			listarDatos[i] = userInput.nextLine();
+		}	
+	}
+	
+	
+public static void main (String [] args) {
+	altaPersona alta1 = new altaPersona();
+	alta1.ingresarDatosPersonales();
+}
+}
